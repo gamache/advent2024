d086110a407c9ebfbf8beff26b7cbb0694b413f3
pete gamache <pete@gamache.org>
2024-12-11 12:02:55 -0500

    day 11 part 2

--- src/day11.rs
+++ src/day11.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 pub fn run(input: &String) {
     let stones: Vec<usize> = input
         .trim()
@@ -9,20 +11,72 @@ pub fn run(input: &String) {
     part2(&stones);
 }
 
-fn part1(start_stones: &Vec<usize>) {
-    let mut stones = start_stones.clone();
-    for _ in 0..25 {
-        stones = blink(&stones);
-    }
-    println!("part 1: {}", stones.len());
+fn part1(stones: &Vec<usize>) {
+    println!("part 1: {}", blink_n(stones, 25).len());
 }
 
-fn part2(start_stones: &Vec<usize>) {
+fn part2(stones: &Vec<usize>) {
+    let mut memo25: HashMap<usize, Vec<usize>> = HashMap::new();
+
+    stones.iter().for_each(|stone| {
+        if None == memo25.get(stone) {
+            memo25.insert(*stone, blink_n(&vec![*stone], 25));
+        }
+    });
+
+    memo25.clone().iter().for_each(|(_, v)| {
+        let set: HashSet<usize> = HashSet::from_iter(v.clone());
+        set.iter().for_each(|stone| {
+            if None == memo25.get(stone) {
+                memo25.insert(*stone, blink_n(&vec![*stone], 25));
+            }
+        });
+    });
+
+    memo25.clone().iter().for_each(|(_, v)| {
+        let set: HashSet<usize> = HashSet::from_iter(v.clone());
+        set.iter().for_each(|stone| {
+            if None == memo25.get(stone) {
+                memo25.insert(*stone, blink_n(&vec![*stone], 25));
+            }
+        });
+    });
+
+    let mut s2memo: HashMap<usize, usize> = HashMap::new();
+
+    let count: usize = stones
+        .iter()
+        .map(|s1| {
+            memo25
+                .get(s1)
+                .unwrap()
+                .iter()
+                .map(|s2| match s2memo.get(s2) {
+                    Some(c) => *c,
+                    None => {
+                        let c = memo25
+                            .get(s2)
+                            .unwrap()
+                            .iter()
+                            .map(|s3| memo25.get(s3).unwrap().len())
+                            .sum::<usize>();
+                        s2memo.insert(*s2, c);
+                        c
+                    }
+                })
+                .sum::<usize>()
+        })
+        .sum();
+
+    println!("part 2: {}", count)
+}
+
+fn blink_n(start_stones: &Vec<usize>, times: usize) -> Vec<usize> {
     let mut stones = start_stones.clone();
-    for _ in 0..75 {
+    for _ in 0..times {
         stones = blink(&stones);
     }
-    println!("part 2: {}", stones.len());
+    stones
 }
 
 fn blink(stones: &Vec<usize>) -> Vec<usize> {
@@ -48,6 +102,5 @@ fn blink(stones: &Vec<usize>) -> Vec<usize> {
 
         new_stones.push(stone * 2024);
     }
-    //println!("{:?}", new_stones);
     new_stones
 }
