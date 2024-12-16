4432d2d1577686defd11bfaca1e55a08bb4da050
pete gamache <pete@gamache.org>
2024-12-16 08:41:57 -0500

    day 16 part 2

--- src/day16.rs
+++ src/day16.rs
@@ -1,6 +1,7 @@
 use std::cmp::Ordering;
 use std::collections::BinaryHeap;
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 use crate::Coord;
 use crate::Grid;
@@ -10,13 +11,13 @@ struct Path {
     pub coord: Coord,
     pub direction: Coord,
     pub cost: usize,
+    pub prev_coords: Vec<Coord>,
 }
 impl Ord for Path {
     fn cmp(&self, other: &Self) -> Ordering {
         other.cost.cmp(&self.cost) // reverse order
     }
 }
-
 impl PartialOrd for Path {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
@@ -25,53 +26,58 @@ impl PartialOrd for Path {
 
 pub fn run(lines: &Vec<String>) {
     let grid = Grid::from_lines(lines);
-    part1(&grid);
-}
-
-fn part1(grid: &Grid) {
-    let mut heap = BinaryHeap::new();
     let start = grid.find("S").unwrap();
+    let mut visited: HashMap<(Coord, Coord), usize> = HashMap::new();
+    let mut win_paths: Vec<Path> = vec![];
+    let mut heap = BinaryHeap::new();
     heap.push(Path {
         coord: start,
         direction: Coord { row: 0, col: 1 },
         cost: 0,
+        prev_coords: vec![],
     });
 
-    let mut visited: HashMap<(Coord, Coord), usize> = HashMap::new();
-    let mut win_path: Option<Path> = None;
     while let Some(path) = heap.pop() {
+        if let Some(win_path) = win_paths.get(0) {
+            if path.cost > win_path.cost {
+                break;
+            }
+        }
         //println!("{:?}", path);
         match grid.coords.get(&path.coord) {
             None => continue,
             Some(s) if s == &String::from("#") => continue,
-            Some(s) if s == &String::from("E") => {
-                win_path = Some(path);
-                break;
-            }
+            Some(s) if s == &String::from("E") => win_paths.push(path),
             _ => {
                 let p = path.clone();
                 let pair = (p.coord, p.direction);
                 match visited.get(&pair) {
-                    Some(cost) if *cost <= path.cost => continue,
+                    Some(cost) if *cost < path.cost => continue,
                     _ => {
                         visited.insert(pair, path.cost);
                     }
                 };
 
+                let mut prev_coords = path.prev_coords.clone();
+                prev_coords.push(path.coord.clone());
+
                 let proceed = Path {
                     coord: path.coord.add(&path.direction, 1),
                     direction: path.direction.clone(),
                     cost: path.cost + 1,
+                    prev_coords: prev_coords.clone(),
                 };
                 let turn_left = Path {
                     coord: path.coord.clone(),
                     direction: path.direction.turn_left(),
                     cost: path.cost + 1000,
+                    prev_coords: prev_coords.clone(),
                 };
                 let turn_right = Path {
                     coord: path.coord.clone(),
                     direction: path.direction.turn_right(),
                     cost: path.cost + 1000,
+                    prev_coords: prev_coords.clone(),
                 };
                 heap.push(proceed);
                 heap.push(turn_left);
@@ -80,5 +86,14 @@ fn part1(grid: &Grid) {
         }
     }
 
-    println!("part 1: {}", win_path.unwrap().cost)
+    println!("part 1: {}", win_paths[0].cost);
+
+    let mut tileset: HashSet<Coord> = HashSet::new();
+    for path in win_paths {
+        tileset.insert(path.coord);
+        for coord in path.prev_coords {
+            tileset.insert(coord);
+        }
+    }
+    println!("part 2: {}", tileset.len());
 }
