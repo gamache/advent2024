63bca83758eb1f1bcbed8b69edce7881bf68d3c1
pete gamache <pete@gamache.org>
2024-12-28 09:22:40 -0500

    run it all

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,22 @@ fn read_lines(filename: &str) -> Vec<String> {
         .collect()
 }
 
+mod day01;
+mod day02;
+mod day03;
+mod day04;
+mod day05;
+mod day06;
+mod day07;
+mod day08;
+mod day09;
+mod day10;
+mod day11;
+mod day12;
+mod day13;
+mod day14;
+mod day15;
+mod day16;
 mod day17;
 mod day18;
 mod day19;
@@ -21,37 +37,31 @@ mod day24;
 mod day25;
 
 fn main() {
-    // day25::run(&read_to_string("inputs/test25.txt").unwrap());
-    // day25::run(&read_to_string("inputs/day25.txt").unwrap());
-
-    // day25::run(&read_to_string("inputs/test25.txt").unwrap());
-    // day24::run(&read_to_string("inputs/day24.txt").unwrap());
-
-    // day23::run(&read_lines("inputs/test23.txt"));
-    // day23::run(&read_lines("inputs/day23.txt"));
-
-    // day22::run(&read_lines("inputs/test22.txt"));
-    // day22::run(&read_lines("inputs/day22.txt"));
-
-    // day21::run(&read_lines("inputs/test21.txt"));
-    // day21::run(&read_lines("inputs/day21.txt"));
-
-    // day20::run(&read_lines("inputs/test20.txt"));
-    // day20::run(&read_lines("inputs/day20.txt"));
-
-    // day19::run(&read_to_string("inputs/test19.txt").unwrap());
-    // day19::run(&read_to_string("inputs/day19.txt").unwrap());
-
-    // day18::run(&read_lines("inputs/test18.txt"));
-    // day18::run(&read_lines("inputs/day18.txt"));
-
-    // day17::run(&read_to_string("inputs/test17.txt").unwrap());
+    day25::run(&read_to_string("inputs/day25.txt").unwrap());
+    day24::run(&read_to_string("inputs/day24.txt").unwrap());
+    day23::run(&read_lines("inputs/day23.txt"));
+    day22::run(&read_lines("inputs/day22.txt"));
+    day21::run(&read_lines("inputs/day21.txt"));
+    day20::run(&read_lines("inputs/day20.txt"));
+    day19::run(&read_to_string("inputs/day19.txt").unwrap());
+    day18::run(&read_lines("inputs/day18.txt"));
     day17::run(&read_to_string("inputs/day17.txt").unwrap());
-    // day17::run("Register A: 0 Register B: 0 Register C: 9 Program: 2,6");
-    // day17::run("Register A: 10 Register B: 0 Register C: 9 Program: 5,0,5,1,5,4");
-    // day17::run("Register A: 2024 Register B: 0 Register C: 9 Program: 0,1,5,4,3,0");
-    // day17::run("Register A: 0 Register B: 29 Register C: 9 Program: 1,7");
-    // day17::run("Register A: 0 Register B: 2024 Register C: 43690 Program: 4,0");
+    day16::run(&read_lines("inputs/day16.txt"));
+    day15::run(&read_to_string("inputs/day15.txt").unwrap());
+    day14::run(&read_lines("inputs/day14.txt"));
+    day13::run(&read_to_string("inputs/day13.txt").unwrap());
+    day12::run(&read_lines("inputs/day12.txt"));
+    day11::run(&read_to_string("inputs/day11.txt").unwrap());
+    day10::run(&read_lines("inputs/day07.txt"));
+    day09::run(&read_to_string("inputs/day09.txt").unwrap());
+    day08::run(&read_lines("inputs/day08.txt"));
+    day07::run(&read_lines("inputs/day07.txt"));
+    day06::run(&read_lines("inputs/day06.txt"));
+    day05::run(&read_to_string("inputs/day05.txt").unwrap());
+    day04::run(&read_lines("inputs/day04.txt"));
+    day03::run(&read_to_string("inputs/day03.txt").unwrap());
+    day02::run(&read_lines("inputs/day02.txt"));
+    day01::run(&read_lines("inputs/day01.txt"));
 }
 
 #[derive(Hash, PartialEq, Eq, Debug, Clone, Copy)]
